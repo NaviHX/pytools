b653e9e81a6c1d89f198d42736fb7d30ba39ece6
navihx <932942142@qq.com>
2021-06-06 18:52:10 +0800

    add name-filter

---
+++ name-filter/name-filter.py
@@ -0,0 +1,24 @@
+from xpinyin import Pinyin
+import argparse
+
+target_initials = ''
+line_count = 0
+
+pinyin = Pinyin()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-t', '--target', required=True, help='Target Initials')
+
+args = parser.parse_args()
+target_initials = args.target
+target_initials = target_initials.upper()
+
+while True:
+    try:
+        name = input()
+        initials = pinyin.get_initials(name, '')
+        if initials == target_initials:
+            print(line_count, ':', name)
+        line_count += 1
+    except Exception as e:
+        break
